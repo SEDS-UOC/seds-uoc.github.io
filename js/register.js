061e9c72ab99d820b92d2d4c5ce1ecf3fed1f769
Naushikha <naushikha.jayawickrama@gmail.com>
2021-05-07 17:19:46 +0530

    Seperated navbar script from landing.js

---
+++ js/register.js
@@ -0,0 +1,7 @@
+// ---------------------------------------------
+// Do activities after loading document
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Add hambug functionality
+  activateHambugs(); // Include navbar.js first!
+});
