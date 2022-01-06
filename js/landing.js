76b4fb0e030935ac8a3a92c2bbffecf1866c8f23
Naween Pasindu <37021586+Naween-Pasindu@users.noreply.github.com>
2022-01-06 14:44:28 +0530

    multi line problem fixed

--- js/landing.js
+++ js/landing.js
@@ -26,12 +26,26 @@ window.onload = async function () {
     if (dataJson !== null) {
       dataJson.forEach((person) => {
         var names = person.name.split(" ");
-        var name = '<p class="title is-4 mb-0">'+names[0]+'</p><p class="title is-4 mt-0">';
-        names.shift();
-        names.forEach(function(value){
-          name += value + '</br>';
-        });
-        name += '</p>';
+        if(names.indexOf("Rev.") < -1){
+          var name = '<p class="title is-4 mb-0">'+names[0]+'</p><p class="title is-4 mt-0">';
+          names.shift();
+          names.forEach(function(value){
+            name += value + '</br>';
+          });
+          name += '</p>';
+        }else{
+          console.log('hello');
+          if(names.length % 2 ==1){
+            names.push(" ");
+          }
+          var name = '<p class="title is-4 mb-0">'+names[0]+' '+names[1]+'</p><p class="title is-4 mt-0">';
+          names.shift();
+          names.shift();
+          for (let i = 0; i < names.length; i+=2) {
+            name += names[i]+' '+names[i+1] + '</br>';
+          }
+          name += '</p>';
+        }
         document.getElementById("exco-cards-holder").innerHTML =
           document.getElementById("exco-cards-holder").innerHTML +
           `
